d0e7aedde570e788b0a2608b2df305cc80d121e3
dodonki1223 <make.an.effort.wish.come.true@gmail.com>
2019-12-02 12:41:43 +0900

    :recycle: 期限付きラベルの対応

--- JiraBackLogSlimDisplay/JiraBackLogSlimDisplay.js
+++ JiraBackLogSlimDisplay/JiraBackLogSlimDisplay.js
@@ -1,8 +1,15 @@
-javascript: (function () {
+javascript: (() => {
   const elems = document.querySelectorAll('span.ghx-extra-field-content');
   setInterval(() => {
     Array.prototype.forEach.call(elems, function (elem) {
       if (elem.innerText == '割り込み') {
+        let target = elem.parentElement.parentElement.parentElement.querySelector('.ghx-inner');
+        target.setAttribute('style', 'color: green;')
+        target.innerText = '★割り込み★ ' + target.innerText.substring('★割り込み★ '.length); 
+      } else if (elem.innerText == '期限付き') {
+        let target = elem.parentElement.parentElement.parentElement.querySelector('.ghx-inner');
+        target.setAttribute('style', 'color: red;')
+        target.innerText = '★期限付き★ ' + target.innerText.substring('★期限付き★ '.length); 
         elem.parentElement.parentElement.parentElement.querySelector('.ghx-inner').setAttribute('style', 'color: red;')
       }
     });
